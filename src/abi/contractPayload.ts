17c97d185144f901ae31e8c70f6b71a9a5549b95
jamie-jungah <wjddk718@gmail.com>
2023-08-14 02:56:24 +0900

    Fix contract address

--- src/abi/contractPayload.ts
+++ src/abi/contractPayload.ts
@@ -3,11 +3,11 @@ import { TEST_ABI } from './testABI';
 type Method = 'setString' | 'getString';
 
 const CONTRACT_RECORD: Record<number, `0x${string}`> = {
-  5: '0xAcDa4bBdb12e9a44E79B655A6Fdf1a63367c0DAa',
-  11155111: '0xAcDa4bBdb12e9a44E79B655A6Fdf1a63367c0DAa',
-  1001: '0xAcDa4bBdb12e9a44E79B655A6Fdf1a63367c0DAa',
-  5001: '0xAcDa4bBdb12e9a44E79B655A6Fdf1a63367c0DAa',
-  97: '0xAcDa4bBdb12e9a44E79B655A6Fdf1a63367c0DAa',
+  5: '0xB9552E2eDe4bB427d78E07c72Fa3Fd337bBEe53c',
+  11155111: '0xDA4b49bdEaeD7dC381f533f6db377A017BCc2CBe',
+  1001: '0xDA4b49bdEaeD7dC381f533f6db377A017BCc2CBe',
+  5001: '0xDA4b49bdEaeD7dC381f533f6db377A017BCc2CBe',
+  97: '0xf393d20a7490b1FDC38382dd34f28994eDAc15ea',
 };
 
 export default function contractPayload(method: Method, chainId?: number) {
